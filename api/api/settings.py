9c4288b7f95f56f80936d5ecb35c16d91deafc69
Yoshioka <sss.yoshioka@gmail.com>
2023-08-28 15:02:04 +0000

    🎸 feat: add default and account to settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "default.apps.DefaultConfig",
+    "account.apps.AccountConfig",
 ]
 
 MIDDLEWARE = [

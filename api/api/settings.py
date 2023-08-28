02287ec836c4f7c39fc26ff7ba0b4a1bc64a0f8d
Yoshioka <sss.yoshioka@gmail.com>
2023-08-28 15:07:49 +0000

    🎸 feat: install bootstrap5
    poetry add django-bootstrap5

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_bootstrap5",
     "default.apps.DefaultConfig",
     "account.apps.AccountConfig",
 ]

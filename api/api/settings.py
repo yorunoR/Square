ce8cb76055a5a69bec4445c904709470bb97e52d
Yoshioka <sss.yoshioka@gmail.com>
2023-09-02 14:27:51 +0000

    🎸 feat: set database

--- api/api/settings.py
+++ api/api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,9 +79,27 @@ WSGI_APPLICATION = "api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "square_dev",
+        "USER": "developer",
+        "PASSWORD": os.getenv("USER_PASSWORD", "secret"),
+        "HOST": os.getenv("DOCKER_POSTGRES_HOST", "localhost"),
+        "PORT": 5432,
+        "TEST": {
+            "NAME": "square_test",
+        },
+    },
+    "superuser": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "square_dev",
+        "USER": "postgres",
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "secret"),
+        "HOST": os.getenv("DOCKER_POSTGRES_HOST", "localhost"),
+        "PORT": 5432,
+        "TEST": {
+            "NAME": "square_test",
+        },
+    },
 }
 
 
